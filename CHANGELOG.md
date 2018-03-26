f7ff950279f7907925935d0cb617878d02a5b51d
Ethras <vladimirmbassi@gmail.com>
2018-03-26 11:16:44 +0200

    Fix for Dart 2.0 compatibility

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.1.1] - 26.03.2018
+
+* Fix for compatibility with Dart 2
+
 ## [0.1.0] - 14.03.2018
 
 * Initial release
--- lib/simple_permissions.dart
+++ lib/simple_permissions.dart
@@ -10,20 +10,23 @@ class SimplePermissions {
       _channel.invokeMethod('getPlatformVersion');
 
   /// Check a [permission] and return a [Future] with the result
-  static Future<bool> checkPermission(Permission permission) {
-    return _channel.invokeMethod(
+  static Future<bool> checkPermission(Permission permission) async  {
+    final bool isGranted = await _channel.invokeMethod(
         "checkPermission", {"permission": getPermissionString(permission)});
+    return isGranted;
   }
 
   /// Request a [permission] and return a [Future] with the result
-  static Future<bool> requestPermission(Permission permission) {
-    return _channel.invokeMethod(
+  static Future<bool> requestPermission(Permission permission) async {
+    final bool isGranted = await _channel.invokeMethod(
         "requestPermission", {"permission": getPermissionString(permission)});
+    return isGranted;
   }
 
   /// Open app settings on Android and iOs
-  static Future<bool> openSettings() {
-    return _channel.invokeMethod("openSettings");
+  static Future<bool> openSettings() async {
+    final bool isOpen = await _channel.invokeMethod("openSettings");
+    return isOpen;
   }
 }
 
