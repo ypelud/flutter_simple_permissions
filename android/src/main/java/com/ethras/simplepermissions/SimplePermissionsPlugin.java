2895c64ddaf9f67c6eb3f17fb256911f9afc3f96
Joseph Cheng <joseph@softworkz.net>
2018-07-05 11:51:41 +0800

    Add READ_SMS permission

--- android/src/main/java/com/ethras/simplepermissions/SimplePermissionsPlugin.java
+++ android/src/main/java/com/ethras/simplepermissions/SimplePermissionsPlugin.java
@@ -109,6 +109,9 @@ public class SimplePermissionsPlugin implements MethodCallHandler, PluginRegistr
             case "READ_CONTACTS":
                 res = Manifest.permission.READ_CONTACTS;
                 break;
+            case "READ_SMS":
+                res = Manifest.permission.READ_SMS;
+                break;
             case "VIBRATE":
                 res = Manifest.permission.VIBRATE;
                 break;
